09939afed65beb691bbafb16e2be9895b3afef00
Rmdhn-20 <76412415+Rmdhn-20@users.noreply.github.com>
2023-07-29 17:38:41 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
-global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
-global.limitCount = 10000
+global.creator = 'Ekuzika OfC'// yourname
+global.MONGO_DB_URI = "mongodb+srv://ekuzika:ekuzika@rest-api.zpr2igw.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
+global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPnew/@10" //isi apa aja bebas
+global.your_email = "tuyullakun12@gmail.com" //email
+global.email_password = "byihxdjzoovknavt" //application password email
+global.limitCount = 999
 global.YUOR_PORT = 8000
 global.loghandler = {
 	noapikey:{
